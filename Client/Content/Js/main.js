2e460605a8331772a3c6d866e6bbfd63f8f774e6
Waleed Haroon <waleedharoon999@gmail.com>
2016-05-23 12:42:02 +0500

    new directory added #tag removed

--- Client/Content/Js/main.js
+++ Client/Content/Js/main.js
@@ -1,4 +1,14 @@
 var myapp = angular.module('main', ["ui.router"])
-myapp.config(function($stateProvider){
-$stateProvider.state('index', {url:"", templateUrl:"Client/Content/template/login.html"});
+myapp.config(function ($stateProvider,$urlRouterProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+    $urlRouterProvider.otherwise("/index");
+    $stateProvider.state('index',
+        {
+            url: "/index",
+            views: {
+                "login": {
+                    templateUrl: "client/content/template/login.html"
+                }
+            }
+        });
 });
